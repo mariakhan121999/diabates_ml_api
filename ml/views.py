3037b40421566508ed72df1e47b2205cbb24872b
mariakhan121999 <122410610+mariakhan121999@users.noreply.github.com>
2023-03-07 21:41:48 -0500

    Update views.py

--- ml/views.py
+++ ml/views.py
@@ -8,3 +8,14 @@ import joblib
 import os
 
 base_dir = os.path.dirname(__file__)
+
+
+# Create your views here.
+@csrf_exempt
+@api_view(['POST'])
+def predict(request, model_name):
+    data = [request.data["input"]]
+
+    load_model = joblib.load(open(base_dir + "/saved_models/" + model_name + ".joblib", 'rb'))
+    y_pred = load_model.predict(data)
+    return JsonResponse(y_pred[0], safe=False)
